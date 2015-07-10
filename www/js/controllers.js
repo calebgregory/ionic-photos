459b8ec1d31a048d26c363c3c34c31bb953cf3f8
calebgregory <calebgregory@gmail.com>
2015-07-10 15:03:12 -0500

    Adding geolocation feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,6 +10,21 @@ angular.module('starter.controllers', [])
 })
 
 
-.controller('LocationCtrl', function($scope) {
+.controller('LocationCtrl', function($scope, $http, Pictures) {
+  // get user's geolocation using their ip address
+  Pictures.geoLoc(function(res) {
+    $scope.lat = res.lat;
+    $scope.lon = res.lon;
+    $scope.bbox = {
+      minLon : res.lon - .006, // make a square of lengths
+      minLat : res.lat - .006, // .2 degrees with the user's
+      maxLon : res.lon + .006, // geolocation as the center
+      maxLat : res.lat + .006,
+      toString : function() { return `${this.minLon},${this.minLat},${this.maxLon},${this.maxLat}`; }
+    };
 
+    Pictures.getPhotos($scope.bbox.toString(), function(res) {
+      $scope.photos = res.photos.photo;
+    });
+  });
 });
